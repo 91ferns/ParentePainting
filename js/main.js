6304b6b595f4e7036b23f59774c7f5d6db594333
Stephen Parente <webmakersteve@gmail.com>
2015-01-14 23:54:02 -0500

    Put the boilerplate in

---
+++ js/main.js
@@ -0,0 +1,22 @@
+require.config({
+  baseUrl: '/',
+  paths: {
+    // the left side is the module ID,
+    // the right side is the path to
+    // the jQuery file, relative to baseUrl.
+    // Also, the path should NOT include
+    // the '.js' file extension. This example
+    // is using jQuery 1.9.0 located at
+    // js/lib/jquery-1.9.0.js, relative to
+    // the HTML page.
+    jquery: 'components/jquery/dist/jquery.min',
+    bootstrap: 'components/bootstrap-sass/assets/javascripts/bootstrap',
+    flexslider: 'components/flexslider/jquery.flexslider-min'
+  }
+});
+
+require(['jquery', 'bootstrap'], function($) {
+
+  
+
+});
