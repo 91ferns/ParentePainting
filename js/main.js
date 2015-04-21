12e6636aa3c98c505fdc722be1404429421e5a4c
Stephen Parente <webmakersteve@gmail.com>
2015-04-21 17:28:37 -0400

    Fixes for mailer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 require.config({
   baseUrl: '/',
+  shim : {
+    "bootstrap" : { "deps" :['jquery'] }
+  },
   paths: {
     // the left side is the module ID,
     // the right side is the path to
@@ -19,6 +22,49 @@ require(['jquery', 'bootstrap'], function($) {
 
   $('#contact-btn').click(function() {
     $('#Modal').modal();
+  });
+
+  $('#ContactForm').submit(function() { //.attr('action', 'javascript:void(0)')
+
+    var ctxt = $(this);
+
+    var name = $('#ContactName', ctxt).val();
+    var email = $('#ContactEmail', ctxt).val();
+    var message = $('#ContactMessage', ctxt).val();
+
+    if (!name) {
+      alert('Please enter your name');
+      return false;
+    } else if (!email) {
+      alert('Please enter your email address');
+      return false;
+    } else if (!message) {
+      alert('Please enter a message');
+      return false;
+    }
+    return true;
+
+    return $.ajax({
+      method: 'POST',
+      dataType: 'jsonp',
+      url: 'http://ferns91.herokuapp.com/api/external/mailer/new',
+      data: {
+        api_mail: {
+          applicaton: 'parente_painting',
+          message: message,
+          from: name + '<' + email + '>',
+          subject: 'Message on ParentePainting.com',
+          additional: 'Sent from parentepainting.com'
+        }
+      },
+      error: function(err) {
+        alert(err);
+      },
+      success: function(d) {
+        console.log(d);
+        $('#Modal').modal('hide');
+      }
+    })
   });
 
 });
